3ff4cd8fd483a096c933be82a91c201443a243af
liyouchen <allen11270624@163.com>
2020-12-17 23:59:48 +0800

    添加array_unique2函数

--- functions.php
+++ functions.php
@@ -1,1 +1,14 @@
 <?php
+if (!function_exists('array_unique2')) {
+	function array_unique2(array $array, $key){
+		if ( is_array( $array ) ) {
+			$result = [];
+			foreach ($array as $k=> $v) {
+				if ( !isset($result[$v[$key]]) ) {
+					$result[$v[$key]] = $v;
+				}
+			}
+			return array_values( $result );
+		}
+	}
+}
